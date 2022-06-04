3ef0087843843bd169c629450f93e75bfc805999
joeyhwang <joeyhwang01@gmail.com>
2022-06-03 22:40:16 -0700

    Fix category query

--- pages/anime/search/[query].tsx
+++ pages/anime/search/[query].tsx
@@ -21,7 +21,7 @@ import AnimeCardGrid from 'components/AnimeCardGrid';
     let data = {};
     if (process.env.NEXT_PUBLIC_CLIENT_ID) {
       const res = await fetch(
-        `https://api.myanimelist.net/v2/anime/ranking?ranking_type=${query}&limit=100&fields=id,title,main_picture,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,media_type,status,genres,num_episodes,start_season,average_episode_duration,studios`,
+        `https://api.myanimelist.net/v2/anime?q=${query}&limit=100&fields=id,title,main_picture,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,media_type,status,genres,num_episodes,start_season,average_episode_duration,studios`,
       {
         method: 'get',
         headers: new Headers({ 'X-MAL-CLIENT-ID': process.env.NEXT_PUBLIC_CLIENT_ID }),
